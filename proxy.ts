83c00233a97f6cd1156ee4e5950d3d50f844579a
Krish <krish12tomar@gmail.com>
2026-08-26 09:36:13 +0530

    Auth and DB setup and Save New User Information to DB

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,27 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export function proxy(request: NextRequest) {
-  return NextResponse.next();
-}
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!_next|[^?]*\\.(?:html|css|js|gif|svg|jpg|jpeg|png|woff|woff2|ico|csv|docx|xlsx|zip|webmanifest)).*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for Clerk's auto-proxy path
+    '/__clerk/:path*',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
+
+
+
+//import { NextResponse } from "next/server";
+//import type { NextRequest } from "next/server";
+
+//export function proxy(request: NextRequest) {
+ // return NextResponse.next();
+//}
+
+//export const config = {
+  //matcher: ["/((?!_next|[^?]*\\.(?:html|css|js|gif|svg|jpg|jpeg|png|woff|woff2|ico|csv|docx|xlsx|zip|webmanifest)).*)"],
+//};
