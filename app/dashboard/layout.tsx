f884d1996f7651ad00a2ecc0ed86d0cd27aa4630
Krish <krish12tomar@gmail.com>
2026-09-14 09:54:39 +0530

    Dashboard Layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/custom/dashboard/AppSideBar'
+import AppHeader from '@/components/custom/dashboard/AppHeader'
+
+function DashboardLayout({ children, }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+       <AppSidebar /> 
+       <div className="flex flex-1 flex-col">
+            <AppHeader />
+            {children} </div>
+    </SidebarProvider>
+  )
+}
+
+export default DashboardLayout
