e7630883d123aedde7d33ddf0236c837fa6cb869
Krish <krish12tomar@gmail.com>
2026-09-14 14:57:19 +0530

    Dashboard Page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,16 @@
+import ProjectList from '@/components/custom/dashboard/ProjectList'
+import WelcomeBanner from '@/components/custom/dashboard/WelcomeBanner'
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function DashboardPage() {
   return (
     <div>
-        <UserButton />
+        {/* Welcome Banner */}
+        <WelcomeBanner />
+
+        {/* Project List / Empty State */}
+        <ProjectList />
     </div>
   )
 }
